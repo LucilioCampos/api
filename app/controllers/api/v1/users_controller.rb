014fc4dd3da5ea8b6897fed0242381f4a39762dd
Lucilio Campos <lucilio_oliveira@hotmail.com>
2019-06-15 11:10:23 -0300

    add create method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,6 +7,22 @@ module Api
         render json: @users
       end
 
+      def created
+        @user = User.new(user_params)
+
+        if @user.save
+          render json: { status: 'SUCCESS', message: 'User saved', data: :@user}, status: :ok
+        else
+          render json: {status: 'ERROR', message:'Articles not saved', data:article.erros},status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def user_params
+        params.permit(:username, :email)
+      end
+
     end
   end
 end
